e0b9b43952ec826f065ee863237a2e018149e4ea
ahmad <ahmadfikririch@gmail.com>
2026-06-19 22:44:21 +0700

    update data product

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -9,39 +9,49 @@ class ProductSeeder extends Seeder
 {
     public function run(): void
     {
-        Product::create([
-            'name' => 'Indomie Goreng',
-            'barcode' => '8992388111111',
-            'price' => 3500,
-            'stock' => 120,
-        ]);
+        $branches = [1, 2, 3, 4, 5];
 
-        Product::create([
-            'name' => 'Aqua 600ml',
-            'barcode' => '8992388222222',
-            'price' => 4000,
-            'stock' => 90,
-        ]);
+        foreach ($branches as $branchId) {
 
-        Product::create([
-            'name' => 'Teh Botol',
-            'barcode' => '8992388333333',
-            'price' => 5000,
-            'stock' => 75,
-        ]);
+            Product::create([
+                'branch_id' => $branchId,
+                'name' => 'Indomie Goreng',
+                'barcode' => '8992388111111-' . $branchId,
+                'price' => 3500,
+                'stock' => 120,
+            ]);
 
-        Product::create([
-            'name' => 'Beras 5Kg',
-            'barcode' => '8992388444444',
-            'price' => 68000,
-            'stock' => 30,
-        ]);
+            Product::create([
+                'branch_id' => $branchId,
+                'name' => 'Aqua 600ml',
+                'barcode' => '8992388222222-' . $branchId,
+                'price' => 4000,
+                'stock' => 90,
+            ]);
 
-        Product::create([
-            'name' => 'Minyak Goreng',
-            'barcode' => '8992388555555',
-            'price' => 18000,
-            'stock' => 40,
-        ]);
+            Product::create([
+                'branch_id' => $branchId,
+                'name' => 'Teh Botol',
+                'barcode' => '8992388333333-' . $branchId,
+                'price' => 5000,
+                'stock' => 75,
+            ]);
+
+            Product::create([
+                'branch_id' => $branchId,
+                'name' => 'Beras 5Kg',
+                'barcode' => '8992388444444-' . $branchId,
+                'price' => 68000,
+                'stock' => 30,
+            ]);
+
+            Product::create([
+                'branch_id' => $branchId,
+                'name' => 'Minyak Goreng',
+                'barcode' => '8992388555555-' . $branchId,
+                'price' => 18000,
+                'stock' => 40,
+            ]);
+        }
     }
 }
